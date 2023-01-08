0668c9d412e5517d25f41204232096db00092f3f
joyscript <joyscript7@gmail.com>
2023-01-08 23:21:53 +0300

    feat: add an important note

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -7,17 +7,25 @@ class GuessingGame {
     }
 
     guess() {
-      this.cur = Math.round((this.min + this.max) / 2);
+      this.cur = Math.round((this.min + this.max) / 2); //Math.floor, see the note below
       return this.cur;
     }
 
     lower() {
-      this.max = this.cur;
+      this.max = this.cur; // this.cur - 1
     }
 
     greater() {
-      this.min = this.cur;
+      this.min = this.cur; // this.cur + 1
     }
 }
 
 module.exports = GuessingGame;
+
+/* Note:
+Solution with Math.round and without reducing the range passes all tests.
+But I think that solution with Math.floor and reducing the range more correct, because:
+1) Math.round without reducing will never find 0;
+2) Math.floor with reducing finds the number faster more often;
+see https://replit.com/@joyscript/Binary-Tree#index.js
+*/
